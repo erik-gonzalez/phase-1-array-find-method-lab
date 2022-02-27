84626bacfe3590f1d3067cd528901bc006945f06
Erick <gonzalez20157@yahoo.com>
2022-02-26 18:40:13 -0600

    commit tree feb8aeaf256ffa15197127492a9e9657c2329667

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+function superbowlWin(record){
+    const answer = record.find(record => record.result === "W");
+    return !!answer ? answer.year :undefined;
+}
+
+console.log(superbowlWin);
